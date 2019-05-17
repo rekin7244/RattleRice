07c7c2d4c4ab57bcf0c32ac8cf8f121fabff83e8
dongko <dongko@DESKTOP-TRT103H>
2019-05-17 15:07:24 +0900

    포인트 환불 처리 트랜잭션 처리 일괄(진행중)

--- src/com/kh/rr/transaction/controller/InsertTransactionServlet.java
+++ src/com/kh/rr/transaction/controller/InsertTransactionServlet.java
@@ -1,11 +1,14 @@
 package com.kh.rr.transaction.controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.kh.rr.transaction.model.service.TransactionService;
 
 @WebServlet("/insert.tr")
 public class InsertTransactionServlet extends HttpServlet {
@@ -16,7 +19,19 @@ public class InsertTransactionServlet extends HttpServlet {
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String point = request.getParameter("point");
 		
+		int result = new TransactionService().insertTransaction(point);
+		
+		if(result > 0) {
+			response.sendRedirect("");
+		}else {
+			request.getRequestDispatcher("views/common/errorPage.jsp")
+				.forward(request, response);
+		}
+		
+		
+		System.out.println(point);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
--- src/com/kh/rr/transaction/model/dao/TransactionDao.java
+++ src/com/kh/rr/transaction/model/dao/TransactionDao.java
@@ -1,5 +1,29 @@
 package com.kh.rr.transaction.model.dao;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.util.Properties;
+
 public class TransactionDao {
+	private Properties prop = new Properties();
+	
+	public TransactionDao() {
+		String fileName = TransactionDao.class
+				.getResource("/sql/matching/transaction-query.properties")
+				.getPath();
+		try {
+			prop.load(new FileReader(fileName));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	public int insertTransaction(Connection con, String point) {
+		
+		
+		return 0;
+	}
 
 }
