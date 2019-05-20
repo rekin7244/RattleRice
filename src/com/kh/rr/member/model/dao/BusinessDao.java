e696619ce9428a200ff49b0752ecc9d9627c261a
Song <Song@SongnakQ>
2019-05-20 14:58:04 +0900

    비즈니스 회원가입부분 수정 쿼리 수정

--- src/com/kh/rr/member/model/dao/BusinessDao.java
+++ src/com/kh/rr/member/model/dao/BusinessDao.java
@@ -1,16 +1,20 @@
 package com.kh.rr.member.model.dao;
 
+import static com.kh.rr.common.JDBCTemplate.close;
+
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.Properties;
 
+import com.kh.rr.member.model.vo.Member;
 import com.kh.rr.member.model.vo.StoreInfo;
-
-import static com.kh.rr.common.JDBCTemplate.*;
+import com.kh.rr.member.model.vo.StoreMenuInfo;
 
 public class BusinessDao {
 	private Properties prop = new Properties();
@@ -31,7 +35,7 @@ public class BusinessDao {
 	public int businessInfoUpdate(Connection con, StoreInfo storeUser) {
 		PreparedStatement pstmt = null;
 		int result = 0;
-		
+		System.out.println(storeUser.getbId());
 		String query = prop.getProperty("updateBusinessInfo");
 		
 		try {
@@ -54,6 +58,92 @@ public class BusinessDao {
 		
 		return result;
 	}
+
+	public int insertStoreInfo(Connection con, StoreInfo storeInfo, Member member) {
+		PreparedStatement pstmt = null;
+		int result = 0;
+		
+		String query = prop.getProperty("insertStoreInfo");
+		
+		System.out.println(member.getUserId());
+		try {
+			pstmt = con.prepareStatement(query);
+			pstmt.setString(1, storeInfo.getCotact());
+			pstmt.setString(2, storeInfo.getLocation());
+			pstmt.setString(3, storeInfo.getOpening_hore());
+			pstmt.setString(4, storeInfo.getIntro());
+			pstmt.setString(5, storeInfo.getBrand());
+			pstmt.setString(6, member.getUserId());
+			
+			result = pstmt.executeUpdate();
+			
+			System.out.println(storeInfo);
+			System.out.println("비즈니스 dao 가게 정보" +  query);
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			close(pstmt);
+		}
+				
+		return result;
+	}
+
+	public int insertMenuInfo(Connection con, StoreMenuInfo storeMenuInfo, Member member) {
+		PreparedStatement pstmt = null;
+		int result = 0;
+		ResultSet rset = null;
+		HashMap<String, Object> hmap = null;
+		
+		String selectQuery = prop.getProperty("selectStoreInfo");
+		System.out.println(selectQuery); 
+		
+		
+		try {
+			pstmt = con.prepareStatement(selectQuery);
+			pstmt.setString(1, member.getUserId());
+			
+			rset= pstmt.executeQuery();
+			hmap = new HashMap<String, Object>();
+			while (rset.next()) {
+				hmap.put("s_id", rset.getInt("S_ID"));
+				hmap.put("s_code", rset.getInt("S_CODE"));
+			}
+			System.out.println("hmap : " + hmap);
+			
+		} catch (SQLException e1) {
+			e1.printStackTrace();
+		} finally {
+			//close(rset);
+		}
+				
+		
+		String query = prop.getProperty("insertMenuInfo");
+		
+		try {
+			pstmt = con.prepareStatement(query);
+			pstmt.setString(1, storeMenuInfo.getMenu());
+			pstmt.setInt(2, storeMenuInfo.getPrice());
+			pstmt.setString(3, storeMenuInfo.getOrigin());
+			//pstmt.setInt(4, hmap.get("s_id"));
+			//pstmt.setInt(5, hmap.get("s_code"));
+			
+			result = pstmt.executeUpdate();
+			
+			System.out.println(storeMenuInfo);
+			System.out.println("비지니스 dao 메뉴" + query);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close(pstmt);
+		}
+		
+		
+		
+		return result;
+	}
+
+	
 
 }
 
