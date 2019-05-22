2e095a5c43ef3f0a5187552556807b275660cce7
INHYO <INHYO@JisuNotebook>
2019-05-22 14:10:45 +0900

    주석제거

--- src/com/kh/rr/member/model/service/MemberService.java
+++ src/com/kh/rr/member/model/service/MemberService.java
@@ -15,48 +15,6 @@ import com.kh.rr.member.model.vo.Member;
 
 public class MemberService {
 
-	// 로그인 처리용 메소드
-	// public Member loginMember(Member requestMember) {
-	// Connection con = getConnection();
-	//
-	// MemberDao mDao = new MemberDao();
-	//
-	// int result = mDao.checkStatus(con, requestMember);
-	//
-	// Member loginUser = new Member();
-	//
-	// if (result == LoginServlet.LOGIN_OK) {
-	// loginUser = mDao.selectMember(con, requestMember);
-	// loginUser.setStatus(LoginServlet.LOGIN_OK);
-	//
-	// } else {
-	// if (result == LoginServlet.WRONG_PASSWORD) {
-	// loginUser.setStatus(LoginServlet.WRONG_PASSWORD);
-	// } else {
-	// loginUser.setStatus(LoginServlet.ID_NOT_EXIST);
-	//
-	// }
-	// }
-	//
-	// return loginUser;
-	// }
-	//
-	// public int joinMember(Member requestMember) {
-	// Connection con = getConnection();
-	//
-	// MemberDao mDao = new MemberDao();
-	//
-	// int result = mDao.insertMember(con, requestMember);
-	//
-	// if (result == JoinServlet.JOIN_OK) {
-	// commit(con);
-	// } else {
-	// rollback(con);
-	// }
-	//
-	// return result;
-	// }
-
 	public Member loginCheck(String userId, String userPwd) {
 		Connection con = getConnection();
 
